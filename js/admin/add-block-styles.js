2563bf2f236004ff1fd851af0fefedb9226325f6
Alex Whitlock <zanderwhitlock@gmail.com>
2023-08-25 19:22:03 +0100

    updates

--- js/admin/add-block-styles.js
+++ js/admin/add-block-styles.js
@@ -1,17 +1,27 @@
+import { registerBlockStyle } from "@wordpress/blocks";
+
 // Theme Buttons via Native Wordpress buttons
-wp.blocks.registerBlockStyle('core/button', { name: 'dark-button', label: 'Dark' });
+registerBlockStyle("core/button", { name: "dark-button", label: "Dark" });
+// registerBlockStyle('core/button', { name: 'large-button', label: 'Large button' });
 
 // Heading / paragraph "larger" styles
-wp.blocks.registerBlockStyle('core/heading', { name: 'larger', label: 'Larger text' });
-wp.blocks.registerBlockStyle('core/paragraph', { name: 'larger', label: 'Larger text' });
+registerBlockStyle("core/heading", { name: "larger", label: "Larger text" });
+registerBlockStyle("core/paragraph", { name: "larger", label: "Larger text" });
+registerBlockStyle("core/paragraph", { name: "x-larger", label: "X-Larger text", });
 
 // Paragraph "smaller" styles
-wp.blocks.registerBlockStyle('core/paragraph', { name: 'smaller', label: 'Smaller text' });
-wp.blocks.registerBlockStyle('core/paragraph', { name: 'smaller--sm', label: 'Smaller text on mobile' });
+registerBlockStyle("core/paragraph", { name: "smaller", label: "Smaller text", });
+registerBlockStyle("core/paragraph", { name: "smaller--sm", label: "Smaller text on mobile", });
 
-// Columns no spacing 
-wp.blocks.registerBlockStyle('core/columns', { name: 'no-gap', label: 'Remove column gap' });
-wp.blocks.registerBlockStyle('core/columns', { name: 'larger-gap', label: 'Larger column gap' });
+// Columns no spacing
+registerBlockStyle("core/columns", { name: "no-gap", label: "Remove column gap" });
+registerBlockStyle("core/columns", { name: "larger-gap", label: "Larger column gap", });
+
+// Group
+registerBlockStyle("core/group", { name: "rounded", label: "Rounded corners" });
 
 // Cover, rounded corners
-wp.blocks.registerBlockStyle('core/cover', { name: 'rounded', label: 'Rounded corners' });
+registerBlockStyle("core/cover", { name: "rounded", label: "Rounded corners" });
+
+// Tables
+registerBlockStyle("core/table", { name: "separated", label: "Separated borders" });
