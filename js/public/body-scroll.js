82c5f279facd43188572bb621d566d1725ac6bf7
Alex Whitlock <zanderwhitlock@gmail.com>
2023-10-11 10:20:15 +0100

    Update scroll functionality

--- js/public/body-scroll.js
+++ js/public/body-scroll.js
@@ -12,29 +12,39 @@ DOMReady(() => {
   let lastScrollY = 0;
   let ticking = false;
   const scrolledClassName = 'scrolled';
-  const bodyTag = document.getElementsByTagName('body')[0];
+  const bodyTag = document.body ?? document.getElementsByTagName('body')[0];
 
 
   function onScroll() {
-    lastScrollY = window.scrollY;
     requestTick();
   }
 
   function requestTick() {
     if (!ticking) {
+      lastScrollY = Math.max(window.scrollY, document.documentElement.scrollTop, bodyTag.scrollTop);
       requestAnimationFrame(update);
       ticking = true;
     }
   }
 
   function update() {
+
+    // Set body 'scrolled' class past header on/off
     if (lastScrollY > 100) {
       bodyTag.classList.add(scrolledClassName);
     } else {
       bodyTag.classList.remove(scrolledClassName);
     }
+
+    // Set a "scrolled amount"
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const ratio = (lastScrollY / scrollHeight) * 5;
+    bodyTag.style.setProperty("--scroll", (Math.min(Math.max(ratio, 0), 5)).toString());
+
+    // Reset timer
     ticking = false;
   }
+
   window.addEventListener('scroll', onScroll, false);
 
 });
